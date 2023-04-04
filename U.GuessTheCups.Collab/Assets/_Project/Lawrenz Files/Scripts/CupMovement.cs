79d1541214e21edeb3700f9bdf0a6561605e699c
Lawrenz Del Rosario <98245945+thegreatwowens@users.noreply.github.com>
2023-04-04 09:20:39 +0800

    Prototyping Files
    *Added Scenes
    *Prefabs
    *Some functional Scripts

---
+++ U.GuessTheCups.Collab/Assets/_Project/Lawrenz Files/Scripts/CupMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ddr.GuessTheCup.All
+{
+    public class CupMovement : MonoBehaviour
+    {
+
+         [SerializeField]
+
+       List<GameObject> positionsGeneratedVectors;
+       
+
+        public void newVector(){
+
+               
+
+        }
+private void Update() {
+
+
+}
+
+    }
+}
